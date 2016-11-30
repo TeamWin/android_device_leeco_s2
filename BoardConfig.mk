925de412f69c2a46742a02b0a5c8639343ab9792
13THWARRIOR <13THWARRIOR@users.noreply.github.com>
2016-11-30 19:11:31 +0530

    s2: offset address change
    * changed from hex of kernel and ramdisk to real offset address

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,8 +42,8 @@ TARGET_HW_DISK_ENCRYPTION := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x00800615 
-BOARD_RAMDISK_OFFSET := 0x00291fa7 
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
