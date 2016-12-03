1369a9356b97db6f458a55cb98c0abef1d65a2b9
Rishabh Rao <admin@rishabhrao.net>
2016-12-03 20:31:59 +0530

    s2:Rename Image.gz-dtb to kernel
    mkbootimg.mk was giving errors

--- omni_s2.mk
+++ omni_s2.mk
@@ -3,4 +3,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
+PRODUCT_COPY_FILES += \
+    device/leeco/s2/kernel:kernel 
+
 PRODUCT_NAME := omni_s2
