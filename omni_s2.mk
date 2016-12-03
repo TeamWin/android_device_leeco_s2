e3b67e450570beb3c3883ec83640dad1b17e19ae
13THWARRIOR <13THWARRIOR@users.noreply.github.com>
2016-12-03 21:17:20 +0530

    s2: rename kernel to Image.gz-dtb
    *we have dtb kernel not simple kernel

--- omni_s2.mk
+++ omni_s2.mk
@@ -4,6 +4,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
 PRODUCT_COPY_FILES += \
-    device/leeco/s2/kernel:kernel 
+    device/leeco/s2/Image.gz-dtb:Image.gz-dtb
 
 PRODUCT_NAME := omni_s2
