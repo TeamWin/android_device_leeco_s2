a7e7c6d5935c61ad4aa3f023646bf8c2f1f17400
13THWARRIOR <13THWARRIOR@users.noreply.github.com>
2016-12-04 09:44:11 +0530

    s2 : inherit device properties from device.mk

--- full_s2.mk
+++ full_s2.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device.mk
-$(call inherit-product, device/xiaomi/kenzo/device.mk)
+$(call inherit-product, device/leeco/s2/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_s2
