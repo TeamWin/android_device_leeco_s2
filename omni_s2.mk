eb42a07cdc2767a864cc6ae7e9605c24be7feb05
13THWARRIOR <13THWARRIOR@users.noreply.github.com>
2016-12-04 09:40:27 +0530

    s2: using proper method to make

--- omni_s2.mk
+++ omni_s2.mk
@@ -3,7 +3,4 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
-PRODUCT_COPY_FILES += \
-    device/leeco/s2/Image.gz-dtb:Image.gz-dtb
-
 PRODUCT_NAME := omni_s2
